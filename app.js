9e58a0029afaba1c546868657b0646ea8d022aa8
Diesmo <diesmoavergis+github@gmail.com>
2020-11-22 18:00:21 +0100

    multi resolution config added

--- app.js
+++ app.js
@@ -32,6 +32,57 @@ const config = {
       }
     ]
   },
+  fission: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        rule: "live/*",
+        model: [
+          {
+            ab: "128k",
+            vb: "1500k",
+            vs: "1280x720",
+            vf: "30",
+          },
+          {
+            ab: "96k",
+            vb: "1000k",
+            vs: "854x480",
+            vf: "24",
+          },
+          {
+            ab: "96k",
+            vb: "600k",
+            vs: "640x360",
+            vf: "20",
+          },
+        ]
+      },
+      {
+        rule: "show/*",
+        model: [
+          {
+            ab: "128k",
+            vb: "1500k",
+            vs: "720x1280",
+            vf: "30",
+          },
+          {
+            ab: "96k",
+            vb: "1000k",
+            vs: "480x854",
+            vf: "24",
+          },
+          {
+            ab: "64k",
+            vb: "600k",
+            vs: "360x640",
+            vf: "20",
+          },
+        ]
+      },
+    ]
+  }
 //   https: {
 //     port: 8443,
 //     key: './privatekey.pem',
